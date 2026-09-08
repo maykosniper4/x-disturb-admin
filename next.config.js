060f2b371d68305e3b56c809af53030224b22912
maykosniper4 <maykosniper4@gmail.com>
2026-09-08 14:33:07 +0300

    fix(build): configure baseUrl and explicit webpack @ path alias

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /**
  * @type {import('next').NextConfig}
  */
+const path = require("path");
+
 const nextConfig = {
   output: "standalone",
   images: {
     domains: ['i.pravatar.cc'],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
   async rewrites() {
     return [
